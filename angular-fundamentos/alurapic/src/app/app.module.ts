60a34cacc95c4e846491ea644033d957f50ad525
Gustavo Furtado de Oliveira Alves <gustavo.foa@gmail.com>
2018-11-22 22:21:24 -0200

    Create a component

--- angular-fundamentos/alurapic/src/app/app.module.ts
+++ angular-fundamentos/alurapic/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotoComponent } from './photo/photo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoComponent
   ],
   imports: [
     BrowserModule,
