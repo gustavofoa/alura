eb9a294c8d09dcee9b6a90dff2f5a9049f344cbd
Gustavo Furtado de Oliveira Alves <gustavo.foa@gmail.com>
2018-12-04 12:48:10 -0200

    part 2 - lesson 2

---
+++ angular-fundamentos/alurapic/src/app/core/token/token.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+const KEY = 'authToken';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+  constructor() { }
+
+  hasToken(){
+    !!this.getToken();
+  }
+
+  setToken(token:string){
+    return window.localStorage.setItem(KEY, token);
+  }
+
+  getToken(){
+    return window.localStorage.getItem(KEY);
+  }
+
+  removeToken(){
+    return window.localStorage.removeItem(KEY);
+  }
+  
+}
