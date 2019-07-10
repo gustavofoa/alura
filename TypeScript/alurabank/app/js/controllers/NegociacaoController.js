ddd0025efa0867408f70f8f23f934eea4bb88f59
Gustavo Furtado de Oliveira Alves <gustavo.foa@gmail.com>
2019-07-10 18:12:34 -0300

    Typescript i: lesson 3

--- TypeScript/alurabank/app/js/controllers/NegociacaoController.js
+++ TypeScript/alurabank/app/js/controllers/NegociacaoController.js
@@ -1,12 +1,17 @@
 class NegociacaoController {
     constructor() {
+        this._negociacoes = new Negociacoes();
+        this._negociacoesView = new NegociacoesView('#negociacoesView');
         this._inputData = document.querySelector('#data');
         this._inputQuantidade = document.querySelector('#quantidade');
         this._inputValor = document.querySelector('#valor');
+        this._negociacoesView.update(this._negociacoes);
     }
     adiciona(event) {
         event.preventDefault(); //Não recarrega a página
         const negociacao = new Negociacao(new Date(this._inputData.value.replace(/-/g, ',')), parseInt(this._inputQuantidade.value), parseFloat(this._inputValor.value));
-        console.log(negociacao);
+        this._negociacoes.adiciona(negociacao);
+        console.log(this._negociacoes);
+        this._negociacoesView.update(this._negociacoes);
     }
 }
--- TypeScript/alurabank/app/ts/controllers/NegociacaoController.ts
+++ TypeScript/alurabank/app/ts/controllers/NegociacaoController.ts
@@ -2,11 +2,14 @@ class NegociacaoController {
     private _inputData:HTMLInputElement;
     private _inputQuantidade:HTMLInputElement;
     private _inputValor:HTMLInputElement;
+    private _negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
 
     constructor(){
         this._inputData = document.querySelector('#data');
         this._inputQuantidade = document.querySelector('#quantidade');
         this._inputValor = document.querySelector('#valor');
+        this._negociacoesView.update(this._negociacoes);
     }
 
     adiciona(event:Event) {
@@ -16,6 +19,8 @@ class NegociacaoController {
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
-        console.log(negociacao);
+        this._negociacoes.adiciona(negociacao);
+        console.log(this._negociacoes);
+        this._negociacoesView.update(this._negociacoes);
     }
 }
