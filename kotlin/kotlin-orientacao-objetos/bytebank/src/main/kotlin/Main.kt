b8b0bd85922fca998a6ea5c4e4f56bb7c9806e49
Gustavo Furtao de Oliveira Alves <gustavo.foa@gmail.com>
2023-05-18 06:13:03 -0300

    Kotlin Aula 1: Primeiros passos

---
+++ kotlin/kotlin-orientacao-objetos/bytebank/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+
+    val titular: String = "Gustavo"
+    val numeroConta: Int = 1000
+    var saldo: Double = 0.0
+    saldo = 100 + 2.0
+    saldo += 200
+
+    println("titular $titular")
+    println("número da conta $numeroConta")
+    println("saldo da conta $saldo")
+
+}
