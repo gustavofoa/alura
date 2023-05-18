f879c6abbf640d4082c36129dc1f04b42c4a685b
Gustavo Furtao de Oliveira Alves <gustavo.foa@gmail.com>
2023-05-18 06:58:13 -0300

    kotlin-orientacao-objetos - Aula 3: Conhecendo a Oriencação a Objetos

--- kotlin/kotlin-orientacao-objetos/bytebank/src/main/kotlin/Main.kt
+++ kotlin/kotlin-orientacao-objetos/bytebank/src/main/kotlin/Main.kt
@@ -1,13 +1,69 @@
 fun main(args: Array<String>) {
+    println("Bem vindo ao Bytebank")
 
-    val titular: String = "Gustavo"
-    val numeroConta: Int = 1000
-    var saldo: Double = 0.0
-    saldo = 100 + 2.0
-    saldo += 200
+    val contaJoao = Conta()
+    contaJoao.titular = "João"
+    val contaMaria = Conta()
+    contaMaria.titular = "Maria"
 
-    println("titular $titular")
-    println("número da conta $numeroConta")
-    println("saldo da conta $saldo")
+    contaJoao.deposita(100.0)
+    contaMaria.deposita(200.0)
 
+    println("Saldo conta Joao ${contaJoao.saldo}")
+    println("Saldo conta Maria ${contaMaria.saldo}")
+
+}
+
+class Conta {
+    var titular = ""
+    var numeroConta = 0
+    var saldo = 0.0
+
+    fun deposita(valor: Double) {
+        this.saldo += valor
+    }
+
+}
+
+fun testaCopiasEReferencias() {
+    val numeroX = 10
+    var numeroY = numeroX
+    numeroY++
+
+    println("numberoX $numeroX")
+    println("numberoY $numeroY")
+
+
+    val contaJoao = Conta()
+    contaJoao.titular = "João"
+    val contaMaria = contaJoao
+    contaMaria.titular = "Maria"
+
+    println("titular conta joao: ${contaJoao.titular}")
+    println("titular conta maria: ${contaMaria.titular}")
+}
+
+fun testaLacos() {
+    for (i in 5 downTo 1 step 2) {
+
+        val titular: String = "Gustavo $i"
+        val numeroConta: Int = 1000 + i
+        var saldo = i + 10.0
+
+        println("titular $titular")
+        println("número da conta $numeroConta")
+        println("saldo da conta $saldo")
+        println()
+
+//    testaCondicoes(saldo)
+
+    }
+}
+
+private fun testaCondicoes(saldo: Double) {
+    when {
+        saldo > 0.0 -> println("conta é positiva")
+        saldo == 0.0 -> println("conta é neutra")
+        else -> println("conta é negativa")
+    }
 }
