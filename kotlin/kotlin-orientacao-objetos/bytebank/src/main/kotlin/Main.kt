24b2f998c821f7bc9c162286180eac03af5731a8
Gustavo Furtao de Oliveira Alves <gustavo.foa@gmail.com>
2023-05-18 07:46:32 -0300

    kotlin-orientacao-objetos - Aula 5: Escrevendo código mais idiomático ao Kotlin

--- kotlin/kotlin-orientacao-objetos/bytebank/src/main/kotlin/Main.kt
+++ kotlin/kotlin-orientacao-objetos/bytebank/src/main/kotlin/Main.kt
@@ -1,26 +1,31 @@
-fun main(args: Array<String>) {
+fun main() {
     println("Bem vindo ao Bytebank")
 
-    val contaJoao = Conta()
-    contaJoao.titular = "João"
-    contaJoao.numeroConta = 1000
-    val contaMaria = Conta()
-    contaMaria.titular = "Maria"
-    contaMaria.numeroConta = 1001
+    val contaJoao = Conta(titular = "João", numero = 1000)
+    println("Conta criada ${contaJoao.titular} - ${contaJoao.numero}")
+    val contaMaria = Conta("Maria", 1001)
+    println("Conta criada ${contaMaria.titular} - ${contaMaria.numero}")
+
+
 
     println("Depositando 100 na conta do João")
     contaJoao.deposita(100.0)
     println("Depositando 200 na conta da Maria")
     contaMaria.deposita(200.0)
-    println("Saldo conta Joao ${contaJoao.saldo}")
+    println("Saldo conta João ${contaJoao.saldo}")
     println("Saldo conta Maria ${contaMaria.saldo}")
+    println()
+
+    println("Tentando depositar -10 na conta do João")
+    contaJoao.deposita(-10.0)
+    println("Saldo conta João ${contaJoao.saldo}")
     println()
 
     println("Sacando 10 da conta do João")
     contaJoao.saca(10.0)
     println("Sacando 50 da conta da Maria")
     contaMaria.saca(50.0)
-    println("Saldo conta Joao ${contaJoao.saldo}")
+    println("Saldo conta João ${contaJoao.saldo}")
     println("Saldo conta Maria ${contaMaria.saldo}")
     println()
 
@@ -28,37 +33,37 @@ fun main(args: Array<String>) {
     contaJoao.saca(500.0)
     println("Tentando sacar 200 da conta da Maria")
     contaMaria.saca(200.0)
-    println("Saldo conta Joao ${contaJoao.saldo}")
+    println("Saldo conta João ${contaJoao.saldo}")
     println("Saldo conta Maria ${contaMaria.saldo}")
     println()
 
     println("Transferindo 50 da conta do João para a conta da Maria")
     var depositoOcorreuComSucesso = contaJoao.transfere(50.0, contaMaria)
     println("Sucesso na transferência? $depositoOcorreuComSucesso")
-    println("Saldo conta Joao ${contaJoao.saldo}")
+    println("Saldo conta João ${contaJoao.saldo}")
     println("Saldo conta Maria ${contaMaria.saldo}")
     println()
 
     println("Transferindo 50 da conta do João para a conta da Maria")
     depositoOcorreuComSucesso = contaJoao.transfere(50.0, contaMaria)
     println("Sucesso na transferência? $depositoOcorreuComSucesso")
-    println("Saldo conta Joao ${contaJoao.saldo}")
+    println("Saldo conta João ${contaJoao.saldo}")
     println("Saldo conta Maria ${contaMaria.saldo}")
     println()
 
 //    contaMaria.saldo = 1002.0 //Não funciona
-    println("Saldo conta Maria ${contaMaria.saldo}")
-    println()
 }
 
-class Conta {
-    var titular = ""
-    var numeroConta = 0
+class Conta(
+    val titular: String,
+    val numero: Int
+) {
     var saldo = 0.0
         private set
 
     fun deposita(valor: Double) {
-        this.saldo += valor
+        if(valor > 0)
+            this.saldo += valor
     }
 
     fun saca(valor: Double) {
@@ -76,49 +81,4 @@ class Conta {
         }
     }
 
-
-
-}
-
-fun testaCopiasEReferencias() {
-    val numeroX = 10
-    var numeroY = numeroX
-    numeroY++
-
-    println("numberoX $numeroX")
-    println("numberoY $numeroY")
-
-
-    val contaJoao = Conta()
-    contaJoao.titular = "João"
-    val contaMaria = contaJoao
-    contaMaria.titular = "Maria"
-
-    println("titular conta joao: ${contaJoao.titular}")
-    println("titular conta maria: ${contaMaria.titular}")
-}
-
-fun testaLacos() {
-    for (i in 5 downTo 1 step 2) {
-
-        val titular: String = "Gustavo $i"
-        val numeroConta: Int = 1000 + i
-        var saldo = i + 10.0
-
-        println("titular $titular")
-        println("número da conta $numeroConta")
-        println("saldo da conta $saldo")
-        println()
-
-//    testaCondicoes(saldo)
-
-    }
-}
-
-private fun testaCondicoes(saldo: Double) {
-    when {
-        saldo > 0.0 -> println("conta é positiva")
-        saldo == 0.0 -> println("conta é neutra")
-        else -> println("conta é negativa")
-    }
 }
